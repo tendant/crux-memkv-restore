b29b6c8836a75e2db9b3a8f5c2c54d8d8dd9ebde
Lei Wang <tendant@gmail.com>
2021-08-20 10:18:11 -0700

    add checkpointer

--- src/app.clj
+++ src/app.clj
@@ -1,8 +1,14 @@
 (ns app
-  (:require [crux.api :as crux]))
+  (:require [crux.api :as crux])
+  (:import java.time.Duration))
 
 (defn start-node []
-  (crux/start-node {}))
+  (crux/start-node {:crux/index-store
+                    {:kv-store {:checkpointer
+                                {:crux/module 'crux.checkpoint/->checkpointer
+                                 :approx-frequency (Duration/ofSeconds 10)
+                                 :store {:crux/module 'crux.checkpoint/->filesystem-checkpoint-store
+                                         :path "./checkpointer"}}}}}))
 
 (defn -main []
   (let [node (start-node)
@@ -16,6 +22,6 @@
               #inst "2018-05-19T08:31:15.966-00:00"]]
     (println "Submit tx")
     (crux/submit-tx node [tx-1 tx-2])
-    (println "Sleep ...")
-    (Thread/sleep (* 1 1000))
+    (println "Wait 11 seconds for generating checkpointer...")
+    (Thread/sleep (* 11 1000))
     (println "Done")))
